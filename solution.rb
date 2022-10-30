96ab31c2d3a9f5d6db1a681c27600048748b2f25
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-30 22:26:03 +0300

    Completed

--- solution.rb
+++ solution.rb
@@ -89,3 +89,28 @@ class Magazine
     end
 end
 
+class Article 
+    attr_accessor :author, :magazine, :title 
+    @@all = []
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine 
+        @title = title
+        @@all << self.title
+    end
+
+    # all method
+    def self.all 
+        @@all
+    end
+
+    # author && magazine method
+    def author 
+        @author.name
+    end
+
+    def magazine
+        @magazine.name
+    end
+
+end
