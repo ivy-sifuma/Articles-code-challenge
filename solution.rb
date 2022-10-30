5c108ee1972f11e40f414cc3f0ea77e0a4777996
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-30 22:21:33 +0300

    Completed

--- solution.rb
+++ solution.rb
@@ -52,3 +52,40 @@ class Magazine
         @contributors = []
         @articles = []
     end
+
+    # add all class methods
+    def self.all 
+        @@all
+    end 
+    def add_contributors(author)
+        @contributors << author
+        author.magazine = self
+    end
+  
+    def contributors 
+        @contributors.map { |author| author.name }
+    end
+  
+    def contributing_authors
+        @contributors.filter do |author|
+            if author.articles.length > 2
+                author.name
+            else
+                "None" 
+            end
+        end
+    end
+  
+    def add_article(article)
+        @articles << article.title 
+    end
+  
+    def article_titles
+        @articles
+    end
+  
+    def find_by_name(name)
+        @articles.find { |art| art.include?(name)}
+    end
+end
+
