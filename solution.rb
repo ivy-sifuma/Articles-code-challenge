ed497f9b396244cc64a5925a6034de1ad27c0a7f
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-30 22:15:15 +0300

    Completed

--- solution.rb
+++ solution.rb
@@ -7,4 +7,23 @@ class Author
         @name = name
         @articles = []  
         @magazines = []
+    end 
+
+     # methods for adding author's articles and magazines independently
+     def add_art(article)
+        @articles << article.title
+        article.author = self
+    end
+  
+    def add_mag(magazine)
+        @magazines << magazine 
+        magazine.author = self
+    end
+    # article && magazine methods
+    def articles
+        @articles
+    end
+  
+    def magazines
+        @magazines
     end
