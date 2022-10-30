c323788207605ddba525206337061f4eaa36850b
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-30 22:13:00 +0300

    Completed

--- solution.rb
+++ solution.rb
@@ -1,1 +1,10 @@
 # Please copy/paste all three classes into this file to submit your solution!
+class Author
+    attr_reader :name
+    attr_accessor :magazine
+  
+    def initialize(name)
+        @name = name
+        @articles = []  
+        @magazines = []
+    end
